3e9df8dbac036d4eeff0ede068e5b50e01da1052
Cyrus Alde <admin@dolephilschool.com>
2026-07-20 12:40:36 +0800

    Initial Commit with Clean Architecture Setup

---
+++ MyApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyApp.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection service, IConfiguration configure)
+        {
+            service.AddDbContext<ApplicationDBContext>(options =>
+            {
+                options.UseSqlServer(configure.GetConnectionString("defaultconnection"));
+            });
+            return service;
+        }
+    }
+}
