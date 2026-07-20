3c4e1a670f6f8218f3c3204d40aa2f9a523db62e
cyrus alde <aldecyrus2001@gmail.com>
2026-07-20 20:50:24 +0800

    updated solution

--- MyApp.Domain/Entities/Departments.cs
+++ MyApp.Domain/Entities/Departments.cs
@@ -8,14 +8,11 @@ namespace MyApp.Domain.Entities
 {
     public class Departments
     {
-        // Primary Key
         public Guid DepartmentId { get; private set; }
 
-        // Properties
         public string DepartmentName { get; private set; } = string.Empty;
         public string? Description { get; private set; }
 
-        // Navigation Properties
         public virtual ICollection<Employees> Employees { get; private set; } = new List<Employees>();
 
     }
